92066f28cfff92ba5e0a9f6fca7c41a0a09a7ea1
Tanh6969 <nguyenvanxuan01011961@gmail.com>
2025-03-26 19:59:32 +0700

    xoa lab01

--- Lab01/bai6.1.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.JOptionPane;
-public class choosingOption{
-    public static void main(String[] args) {
-    int option = JOptionPane.showConfirmDialog(null,"Do you want to change to the first class ticket ?","Choose your option:" ,JOptionPane.YES_NO_OPTION);
-    String message = "You've chosen: "+ (option == JOptionPane.YES_NO_OPTION ? "Yes" : "No");
-    JOptionPane.showMessageDialog(null, message);
-    System.exit(0);
-    }
-}
